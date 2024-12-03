843a02ac3504439485983ffe1a70471abd6c5b97
Matheus3007 <matheusrp2009@gmail.com>
2024-12-03 02:27:12 -0300

    Add deploy script for github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,15 +12,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://team-micras.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'team-micras', // Usually your GitHub org/user name.
+  projectName: 'team-micras.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
