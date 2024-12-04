9015ea7ad7199d2c73b7625010118f9f6323135c
Matheus Rezende <matheus.rezende@rgspartners.com.br>
2024-12-03 21:37:41 -0300

    fixes all

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'team-micras', // Usually your GitHub org/user name.
   projectName: 'MicrasSite', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   
 
   onBrokenLinks: 'throw',
